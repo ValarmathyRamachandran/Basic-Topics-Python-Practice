2a68544af05e1b6bf5fce848d1950f637f5a235e
Valarmathy <valarmathy.ramachandran@outlook.com>
2022-03-21 23:08:05 +0530

    [Valarmathy]: Implemented core programs of python

---
+++ basic_core_programs/user_input.py
@@ -0,0 +1,9 @@
+def user_function():
+    user_input = input("Please enter your Name here: " )
+    print("Hello " +  user_input + ", How are you? " )
+
+def main():
+    user_function()
+
+main()
+
